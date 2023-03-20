94898bb33f721f51a1245fd131f884a0c748711b
Alex <alexlili0902@gmail.com>
2023-03-20 15:26:26 +0700

    нужно всю папку запушить, а не только cs файл?

---
+++ Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloName
+{ 
+    public class Program 
+    { 
+        public static void Main() 
+        { 
+           SayHello(); 
+        } 
+ 
+        public static void SayHello () 
+        { 
+            Console.WriteLine ("Write your name here: "); 
+            var name = Console.ReadLine (); 
+            Console.WriteLine (name + ", Hello!"); 
+        } 
+    } 
+}
